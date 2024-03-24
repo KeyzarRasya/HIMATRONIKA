afdc0ae7bea7259af99b5152e69232d7f23418b3
KeyzarRasya <keyzar.r.athallah@gmail.com>
2024-03-24 17:11:22 +0700

    vercel

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
 
 app.use(express.static('views'))
 
 app.set('view engine', "ejs")
+app.set('views', path.join(__dirname, 'views'));
 
 app.get("/", (req, res) => {
     res.render('home')
